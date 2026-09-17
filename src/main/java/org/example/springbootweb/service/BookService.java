a7527e044cde18279e8a0cae6090a848ddb4fb52
obvly <obvly@obvlys-MacBook-Air.local>
2026-09-17 15:28:08 +0200

    Add DTOs, MapStruct mappers and BookController

---
+++ src/main/java/org/example/springbootweb/service/BookService.java
@@ -0,0 +1,11 @@
+package org.example.springbootweb.service;
+
+import org.example.springbootweb.dto.BookDto;
+import org.example.springbootweb.dto.CreateBookRequestDto;
+import java.util.List;
+
+public interface BookService {
+    BookDto createBook(CreateBookRequestDto bookDto);
+    List<BookDto> getAll();
+    BookDto getBookById(Long id);
+}
