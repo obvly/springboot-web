822f4e5071ad55a6a9ac40d9c2e86dc19abd213a
obvly <obvly@obvlys-MacBook-Air.local>
2026-09-17 15:47:28 +0200

    Complete homework according to requirements

---
+++ src/main/java/org/example/springbootweb/dto/CreateBookRequestDto.java
@@ -0,0 +1,14 @@
+package org.example.springbootweb.dto;
+
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Data
+public class CreateBookRequestDto {
+    private String title;
+    private String author;
+    private String isbn;
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+}
---
+++ src/main/java/org/example/springbootweb/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package org.example.springbootweb.repository;
+
+import org.example.springbootweb.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
